dbd5c5a32ed923ab685ae05a6e2c1dec5208a6dd
theFoodle <goosetherumfoodle@gmail.com>
2015-07-02 00:29:39 -0400

    kill some useless whitespace

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!, except: [:index, :show, :hello]
-  
+
   def hello
-    
     @opportunities = Opportunity.all
     render 'dashboard/opportunities'
   end
