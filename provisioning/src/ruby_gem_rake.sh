3420ee4f1aa5e76b09242ccf85266f5ef4d432ce
Chad Whitacre <chad@zetaweb.com>
2015-11-23 10:30:06 -0500

    Drop back to Ruby 2.1.4
    This is what is specific in .ruby-version.

--- provisioning/src/ruby_gem_rake.sh
+++ provisioning/src/ruby_gem_rake.sh
@@ -2,9 +2,9 @@
 
 if !(which ruby >/dev/null); then
   cd /tmp
-  wget http://ftp.ruby-lang.org/pub/ruby/2.1/ruby-2.1.5.tar.gz 
-  tar -xvzf ruby-2.1.5.tar.gz
-  cd ruby-2.1.5/
+  wget http://ftp.ruby-lang.org/pub/ruby/2.1/ruby-2.1.4.tar.gz 
+  tar -xvzf ruby-2.1.4.tar.gz
+  cd ruby-2.1.4/
   sudo ./configure --prefix=/usr/local
   sudo make
   sudo make install
