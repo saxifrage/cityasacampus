09bb778cef035b30b79487172cd35eb14e326a40
Matthew Vita <matthewvita@live.com>
2015-11-22 21:51:59 -0500

    no need for conditionals in the system apt-get script

--- provisioning/src/system.sh
+++ provisioning/src/system.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-if [ ! -d "/home/{{home_user}}/cityasacampus" ]; then
-  sudo apt-get update -y
-
-  sudo apt-get install -y \
-    build-essential\
-    python-software-properties \
-    curl \
-    sqlite3 \
-    libsqlite3-dev \
-    libxml2-dev \
-    libxslt1-dev \
-    libreadline-dev \
-    libyaml-dev \
-    libcurl4-openssl-dev \
-    libncurses5-dev \
-    libgdbm-dev \
-    libffi-dev \
-    zlib1g-dev \
-    libssl-dev
-fi
+sudo apt-get install -y \
+  build-essential\
+  python-software-properties \
+  curl \
+  sqlite3 \
+  libsqlite3-dev \
+  libxml2-dev \
+  libxslt1-dev \
+  libreadline-dev \
+  libyaml-dev \
+  libcurl4-openssl-dev \
+  libncurses5-dev \
+  libgdbm-dev \
+  libffi-dev \
+  zlib1g-dev \
+  libssl-dev
