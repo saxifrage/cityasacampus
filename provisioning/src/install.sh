341a1976c093ba0997c397da1b2fcd080b288865
Chad Whitacre <chad@zetaweb.com>
2015-11-23 10:39:56 -0500

    Move top-level install from README to a script

---
+++ provisioning/src/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt-get update -y &&
+sudo apt-get install -y git-core &&
+git clone https://github.com/saxifrage/cityasacampus.git /home/ubuntu/cityasacampus &&
+cd /home/ubuntu/cityasacampus/provisioning &&
+sed -i -- 's/{{home_user}}/ubuntu/g' ./src/* && #{{home_user}} is replaced in files with 'ubuntu'
+./src/system.sh &&
+./src/node_npm_gulp.sh &&
+./src/ruby_gem_rake.sh &&
+./src/rails.sh &&
+./src/bundler.sh &&
+./src/postgres.sh {deployuser} {password} && #fill these variables in
+./src/project.sh {appsecret} {apptoken} && #fill these variables in
+./src/nginx.sh &&
+./src/puma.sh
