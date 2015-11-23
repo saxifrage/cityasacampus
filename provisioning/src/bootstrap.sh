66759c3c438247c717c87dd6b3abb597652a5dc6
Chad Whitacre <chad@zetaweb.com>
2015-11-23 12:27:34 -0500

    Write boostrap.sh to recurse under non-root

--- provisioning/src/bootstrap.sh
+++ provisioning/src/bootstrap.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
+set -e
 
-sudo apt-get update -y &&
-sudo apt-get install -y git-core &&
-git clone https://github.com/saxifrage/cityasacampus.git ~/cityasacampus &&
-cd ~/cityasacampus/provisioning &&
-./src/install.sh
+URL="https://raw.githubusercontent.com/saxifrage/cityasacampus/deployment/provisioning/src/bootstrap.sh"
+
+_recurse() {
+  pass=`cat /dev/urandom | tr -cd 'a-f0-9' | head -c 8`
+  echo "You are root. Setting up a caac user with password '$pass' ..."
+  useradd -m caac -G sudo
+  echo "caac:$pass" | chpasswd
+  curl $URL | sudo -u caac bash
+}
+
+_install() {
+  echo "Installing as $(whoami) ..."
+  sudo apt-get update -y &&
+  sudo apt-get install -y git-core &&
+  git clone https://github.com/saxifrage/cityasacampus.git ~/cityasacampus &&
+  cd ~/cityasacampus/provisioning &&
+  ./src/install.sh
+}
+
+if [ "$(id -u)" == "0" ]; then
+  _recurse
+else
+  _install
+fi
