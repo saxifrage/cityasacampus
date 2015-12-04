818e7a03ed60f63a8a62ec8ca63e45938de4efa5
Chad Whitacre <chad@zetaweb.com>
2015-12-04 17:02:47 -0500

    Stub out Angular files for pathways creation

---
+++ client/dashboard/PathwaysController.js
@@ -0,0 +1,18 @@
+angular.module('caac.pathways.controller', [
+  'caac.shared.navbar.directive',
+  'caac.shared.copyright.directive',
+  'caac.shared.title.service',
+]).controller('PathwaysController', ['$log', '$scope', 'TitleService',
+  function($log, $scope, TitleService) {
+    var self = $scope;
+    var logger = $log.getInstance('PathwaysController');
+
+    self.construct = function() {
+      TitleService.set('Pathways - Dashboard');
+
+      logger.info('loading pathways interface');
+    };
+
+    self.construct();
+  }
+]);
