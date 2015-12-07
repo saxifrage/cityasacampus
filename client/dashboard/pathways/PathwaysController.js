aac740bb0fea63739c5ca467692cbb8e5a62fd31
David Troyer <dmtroyer@gmail.com>
2015-12-07 14:46:43 -0500

    adding the dndLists module as a dependency in the pathways controller

--- client/dashboard/pathways/PathwaysController.js
+++ client/dashboard/pathways/PathwaysController.js
@@ -2,6 +2,7 @@ angular.module('caac.dashboard.pathways.controller', [
   'caac.shared.navbar.directive',
   'caac.shared.copyright.directive',
   'caac.shared.title.service',
+  'dndLists'
 ]).controller('PathwaysController', ['$log', '$scope', 'TitleService',
   function($log, $scope, TitleService) {
     var self = $scope;
