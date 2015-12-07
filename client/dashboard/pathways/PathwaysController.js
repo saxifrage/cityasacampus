443ee8351b1bd0c90fbc39440c1faef99ba0e3f9
Chad Whitacre <chad@zetaweb.com>
2015-12-07 16:12:37 -0500

    Populate the library of opportunities

--- client/dashboard/pathways/PathwaysController.js
+++ client/dashboard/pathways/PathwaysController.js
@@ -3,18 +3,14 @@ angular.module('caac.dashboard.pathways.controller', [
   'caac.shared.copyright.directive',
   'caac.shared.title.service',
   'dndLists'
-]).controller('PathwaysController', ['$log', '$scope', 'TitleService',
-  function($log, $scope, TitleService) {
+]).controller('PathwaysController', ['$log', '$scope', '$http', 'TitleService',
+  function($log, $scope, $http, TitleService) {
     var self = $scope;
     var logger = $log.getInstance('PathwaysController');
 
     TitleService.set('Pathways - Dashboard');
     logger.info('loading pathways interface');
 
-    self.library = [ {label: 'Opportunity'}
-                   , {label: 'Instances'}
-                   , {label: 'Go'}
-                   , {label: 'Here'}
-                    ]
+    $http.get('opportunity_instances.json').then(function(res){ self.library = res.data.result; });
   }
 ]);
