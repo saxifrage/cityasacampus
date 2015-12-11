26b1adeba84b3601c7b1c270f7df94476816a4b2
Chad Whitacre <chad@zetaweb.com>
2015-12-11 10:41:37 -0500

    Wire up pathway drill-down

--- client/dashboard/map/MapController.js
+++ client/dashboard/map/MapController.js
@@ -12,11 +12,41 @@ angular.module('caac.dashboard.map.controller', [
     TitleService.set('Map - Dashboard');
     logger.info('loading map interface');
 
-    self.topics = [{ name: "Cheese"
-                   , pathways: [{ name: "Cheddar"
-                                , resources: []
-                                 }]
-                    }];
+    self.topics = [ { name: "Cheese"
+                    , pathways: [{ name: "Cheddar"
+                                 , resources: [ {name: "Mild"}
+                                              , {name: "Sharp"}
+                                              , {name: "Extra Sharp"}
+                                               ]
+                                  }]
+                     }
+                  , { name: "Bread"
+                    , pathways: [ { name: "Mancini's"
+                                  , resources: [ {name: "Loaf"}
+                                               , {name: "Hoagie Sticks"}
+                                               , {name: "Rolls"}
+                                                ]
+                                   }
+                                , { name: "Breadworks"
+                                  , resources: [ {name: "Wheat"}
+                                               , {name: "Rye"}
+                                               , {name: "Baguette"}
+                                                ]
+                                   }
+                                 ]
+                     }
+                   ];
+
+    self.currentTopic = null;
+    self.selectTopic = function(topic) {
+        self.currentTopic = topic;
+    };
+
+    self.currentPathway = null;
+    self.selectPathway = function(pathway) {
+        self.currentPathway = pathway;
+    };
+
     OpportunityInstancesService.load(function(opportunity_instances){
         self.library = opportunity_instances;
     });
